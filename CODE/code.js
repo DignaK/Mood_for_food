a7cee11d6bce674aa0ab57b6ada4957b87701bfa
DignaK <87437852+DignaK@users.noreply.github.com>
2021-09-27 20:49:50 +0300

    Searchable ingredients in Shopping

--- CODE/code.js
+++ CODE/code.js
@@ -20,26 +20,17 @@ const searchField = document.querySelector("#search");
 const searchResultsContainer = document.querySelector("div.all-ingredients");
 
 searchField.addEventListener('input', (e) => {
-  
-  // if input field is empty, clear the search results
-//   if(e.target.value === '') {
-//      searchResultsContainer.innerHTML = '';
-//      return;
-//   }
-  
-  // filter the meals array
-  const searchResults = ingredients.filter(ingredients => {
-      return ingredients.toLowerCase().includes(e.target.value.toLowerCase());
-  });
-  
-  // before displaying the search results, clear the search results div
-  searchResultsContainer.innerHTML = '';
-  
-  // display the titles of the meal objects that include the text entered in input field
-  searchResults.forEach((element, index) => {
-     let h2 = document.createElement('h2');
-     h2.textContent = element;
-     h2.className = "ingredients";
-     searchResultsContainer.appendChild(h2);
+// filter the ingredients array
+const searchResults = ingredients.filter(ingredients => {
+    return ingredients.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+// before displaying the search results, clear the search results div
+searchResultsContainer.innerHTML = '';
+// display ingredients that include the text entered in input field
+searchResults.forEach(element => {
+    let h2 = document.createElement('h2');
+    h2.textContent = element;
+    h2.className = "ingredients";
+    searchResultsContainer.appendChild(h2);
   });
 });
