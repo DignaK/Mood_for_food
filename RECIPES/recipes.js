657a798f92632e1bcc34112163cb641c35bb21a4
zanepeterena <87437878+zanepeterena@users.noreply.github.com>
2021-09-27 22:29:37 +0300

    Add user recipe, need to add local storage

--- RECIPES/recipes.js
+++ RECIPES/recipes.js
@@ -6,55 +6,105 @@ $(".expand").click(function() {
     $(this).parent().next(".description").toggle();
     
 });
-
-
-
-
-
-//ADD INGREDIENTS FOR USER RECIPE
-$(document).ready(function(){
-  let maxField = 10; //Input fields increment limitation
-  let addButton = $('.add_button'); //Add button selector
-  let wrapper = $('.addIngredients'); //Input field wrapper
-  let fieldHTML = '<div><input type="text" name="newIngredients[]" value=""/><a href="javascript:void(0);" class="remove_button"><img src="IMAGES/minus.png" width= 15px/></a></div>'; //New input field html 
-  let x = 1; //Initial field counter is 1
-  
-  //Once add button is clicked
-  $(addButton).click(function(){
-      //Check maximum number of input fields
-      if(x < maxField){ 
-          x++; //Increment field counter
-          $(wrapper).append(fieldHTML); //Add field html
-      }
-  });
-  
-  //Once remove button is clicked
-  $(wrapper).on('click', '.remove_button', function(e){
-      e.preventDefault();
-      $(this).parent('div').remove(); //Remove field html
-      x--; //Decrement field counter
-  });
 });
 
 
 
+$(document).ready(function() {
 
+    let iCnt = 0;
+    // CREATE A "DIV" ELEMENT AND DESIGN IT USING jQuery ".css()" CLASS.
+    let container = $(document.createElement('div'));
 
+    $('#btAdd').click(function() {
+        if (iCnt <= 19) {
+
+            iCnt = iCnt + 1;
+
+            // ADD TEXTBOX.
+            $(container).append('<input type=text class="input" id=tb' + iCnt + ' ' +
+                 iCnt + '" />');
+
+            // SHOW SUBMIT BUTTON IF ATLEAST "1" ELEMENT HAS BEEN CREATED.
+            if (iCnt == 1) {
+                var divSubmit = $(document.createElement('div'));
+                $(divSubmit).append('<input type=button class="bt"' + 
+                    'onclick="GetTextValue()"' + 
+                        'id=btSubmit value=Add />');
+            }
+
+            // ADD BOTH THE DIV ELEMENTS TO THE "main" CONTAINER.
+            $('#newIngredients').after(container, divSubmit);
+        }
+        // AFTER REACHING THE SPECIFIED LIMIT, DISABLE THE "ADD" BUTTON.
+        // (20 IS THE LIMIT WE HAVE SET)
+        else {      
+            $(container).append('<label>Reached the limit</label>'); 
+            $('#btAdd').attr('class', 'bt-disable'); 
+            $('#btAdd').attr('disabled', 'disabled');
+        }
+    });
+
+    // REMOVE ONE ELEMENT PER CLICK.
+    $('#btRemove').click(function() {
+        if (iCnt != 0) { $('#tb' + iCnt).remove(); iCnt = iCnt - 1; }
+    
+        if (iCnt == 0) { 
+            $(container)
+                .empty() 
+                .remove(); 
+
+            $('#btSubmit').remove(); 
+            $('#btAdd')
+                .removeAttr('disabled') 
+                .attr('class', 'bt');
+        }
+    });
+
+    // REMOVE ALL THE ELEMENTS IN THE CONTAINER.
+    $('#btRemoveAll').click(function() {
+        $(container)
+            .empty()
+            .remove(); 
+
+        $('#btSubmit').remove(); 
+        iCnt = 0; 
+        
+        $('#btAdd')
+            .removeAttr('disabled') 
+            .attr('class', 'bt');
+    });
 });
 
+// PICK THE VALUES FROM EACH TEXTBOX WHEN "SUBMIT" BUTTON IS CLICKED.
+let divValue, values = '';
 
+function GetTextValue() {
+    $(divValue) 
+        .empty() 
+        .remove(); 
+    
+    values = '';
 
+    $('.input').each(function() {
+        divValue = $(document.createElement('div'));
+        values += this.value + '<br />'
+    });
+
+    $(divValue).append(values);
+    $("#ingredientsOfAddedRecipe").append(divValue);
+  }
 
 //ADD TITLE AND INSTRUCTIONS FOR USER RECIPE
-function add(){ 
+function addTitle(){ 
   let newTitle= document.getElementById("newTitle").value;
   document.getElementById("titleOfAddedRecipe").innerHTML=newTitle;
   // console.log("New title:"+ newTitle); 
+};
   
-  // let newIngredients= document.getElementById("newIngredients[]").value; 
-  // console.log("New ingredients:"+ newIngredients); 
-  
+function addInstructions(){ 
   let newInstructions= document.getElementById("newInstructions").value; 
   document.getElementById("instructionsOfAddedRecipe").innerHTML=newInstructions;
   // console.log("New instructions:"+ newInstructions); 
 };
+  
